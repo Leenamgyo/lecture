e1d01ffbea4304354f5a9c6aa925828bb5385d06
unknown <namkyo361@inu.ac.kr>
2022-09-22 22:48:36 +0900

    2022-09-22

--- udemy/python3_begin/section9_application/105_simple_app/main_class_structure.py
+++ udemy/python3_begin/section9_application/105_simple_app/main_class_structure.py
@@ -1,19 +1,40 @@
 class FileManager:
-    def __init__(self):
-        self._items: list 
-        
-    def read_csv(self, path):
-        pass
+    DEFAULT_FILE_PATH = 'rangking.csv'
     
-    def store_csv(self, path):
-        pass
+    def __init__(self):
+        self._file = None
+        
+    def read_csv(self, path=None):
+        path = path if path is not None else FileManager.DEFAULT_FILE_PATH
+        
+        with open(path, 'w') as buffer:
+            self._file = buffer.read()
+            print('read file')
+
+    def store_csv(self, contents, path=None):
+        path = path if path is not None else FileManager.DEFAULT_FILE_PATH
+        
+        with open(path, 'r') as buffer:
+            buffer.write(contents)
+            print('save file')
 
 class Form:
-    def __init__(self, name):
-        self._name: str
-        self._items: dict[str, str]
-            
-    def ask_question(self, template):
+    def __init__(self, data):
+        self._name: str = self.name(None)
+        self._items: dict[str, str] = self.load_data(data)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        print("이름을 입력하세요")
+        
+        if self._name is None:
+            self._name = input()
+    
+    def load_data(self, data):
         pass
     
     
@@ -25,14 +46,18 @@ class Items:
         pass            
 
 if __name__ == '__main__':
-    pass
-
+    fm = FileManager()
+    form = Form('')
+    
+    csv = fm.read_csv()
     # 데이터를 로드할 것 
     # 이름을 입력받을 것
+    form.
     # 질문을 할 것 
     # 체크를 할 것 
     # 추가를 할 것 
     # 저장을 할 것 
+    fm.store_csv(None)
     
         
     
